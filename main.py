9668b7be53782b1ba23e729c19dbf2018988ef98
gauravbasu03 <gauravbasu03@gmail.com>
2024-03-12 22:27:43 -0700

    continued with top 5 most efficient players

--- main.py
+++ main.py
@@ -6,4 +6,12 @@ import numpy as np
 test = 'https://stats.nba.com/stats/leagueLeaders?LeagueID=00&PerMode=PerGame&Scope=S&Season=2023-24&SeasonType=Regular%20Season&StatCategory=PTS'
 
 r = requests.get(url=test).json()
-print(r)
+headers = r['resultSet']['headers']
+data = r['resultSet']['rowSet']
+#print(data)
+headers2 = ['Rank','Player','min','fg_pct','ft_pct','pts']
+df1 = data[0][2]
+for i in range(5):
+    print(data[i][2])
+    print(data[i][7])
+
